2620515f23d9d64f5c9a94607f5db5d185bf34a3
ILikePapyrus <138293023+ILikePapyrus@users.noreply.github.com>
2024-03-06 13:40:45 +0100

    Commit #2 - Lezione [06/03/24]

--- src/EventoMouse.java
+++ src/EventoMouse.java
@@ -50,22 +50,22 @@ public class EventoMouse implements MouseListener {
     public void mouseEntered(MouseEvent e) {
         // Array di stringhe
         String caratteri[] = {"a", "b", "c", "d", "e", "1", "2", "3", "4", "5", "A", "B", "C", "D", "E"};
-        
+
         // Cicla nei pulsanti della matrice
         for(int i=0; i<10; i++) {
             for(int k=0; k<10; k++) {
                 // Controlla da dove viene l'evento
                 if(e.getSource() == matriceBottoni[i][k]) {
-
+                    coloraCornice(i, k, true);
                         // Cambia il testo del pulsante con un carattere a caso preso dall'array caratteri[]
-                        matriceBottoni[i-1][k-1].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i-1][k].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i-1][k+1].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i][k-1].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i][k+1].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i+1][k-1].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i+1][k].setText(caratteri[random.nextInt(caratteri.length)]);
-                        matriceBottoni[i+1][k+1].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i-1][k-1].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i-1][k].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i-1][k+1].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i][k-1].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i][k+1].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i+1][k-1].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i+1][k].setText(caratteri[random.nextInt(caratteri.length)]);
+//                        matriceBottoni[i+1][k+1].setText(caratteri[random.nextInt(caratteri.length)]);
 
 
                     // Colori
@@ -80,6 +80,22 @@ public class EventoMouse implements MouseListener {
         }
     }
 
+    void coloraCornice(int i, int j, boolean on)
+    {
+        for(int a = -1; a < 2; a++)
+        {
+            for (int b = -1; b < 2; b++)
+            {
+                if(!(i == 0 && j == 0) && i+a >= 0 && i+a < matriceBottoni.length && j+b >= 0 && j+b < matriceBottoni[0].length )
+                // matriceBottoni[i+a][j+b].setBackground(on ? Color.BLUE : null);
+                // on ? Color.BLUE : null
+                // Se il mouse è sul pulsante allora colora di blu, altrimenti rimette a posto -> è un if
+
+                matriceBottoni[i+a][j+b].setBackground(on ? Color.BLUE : null);
+            }
+        }
+    }
+
     @Override
     public void mouseExited(MouseEvent e) {
         // Cicla nei pulsanti della matrice
@@ -87,15 +103,8 @@ public class EventoMouse implements MouseListener {
             for(int k=0; k<10; k++) {
                 // Controlla da dove viene l'evento
                 if(e.getSource() == matriceBottoni[i][k]) {
-                    // Cambia il testo del pulsante con un carattere a caso preso dall'array caratteri[]
-                    matriceBottoni[i-1][k-1].setText("");
-                    matriceBottoni[i-1][k].setText("");
-                    matriceBottoni[i-1][k+1].setText("");
-                    matriceBottoni[i][k-1].setText("");
-                    matriceBottoni[i][k+1].setText("");
-                    matriceBottoni[i+1][k-1].setText("");
-                    matriceBottoni[i+1][k].setText("");
-                    matriceBottoni[i+1][k+1].setText("");
+                    coloraCornice(i, k, false);
+
                 }
             }
         }
